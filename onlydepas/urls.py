5b1fc31025f369448c7a4853dbfa371329dd44c4
maryvi <mcamachog@gmail.com>
2024-05-16 21:02:24 +0100

    base --> autenticacion

---
+++ onlydepas/urls.py
@@ -0,0 +1,10 @@
+# urls.py
+from django.contrib import admin
+from django.urls import path, include
+from app.views import index
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', index, name='index'),
+]
